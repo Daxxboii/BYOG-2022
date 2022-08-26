a882c5057b85399773027525c7700940b4b05cee
ActionKnight <66257396+ActionKnight@users.noreply.github.com>
2022-08-26 22:04:41 +0530

    assests

---
+++ BYOG/Assets/BOXOPHOBIC/The Vegetation Engine/Core/Editor/TVEHubAutorun.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+
+namespace TheVegetationEngine
+{
+    [InitializeOnLoad]
+    public class TVEHubAutorun
+    {
+        static TVEHubAutorun()
+        {
+            EditorApplication.update += OnInit;
+        }
+
+        static void OnInit()
+        {
+            EditorApplication.update -= OnInit;
+            TVEHub window = EditorWindow.GetWindow<TVEHub>(false, "The Vegetation Engine", true);
+            window.Show();
+        }
+    }
+}
+
